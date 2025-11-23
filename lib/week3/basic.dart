769f9a6dbe3a2594d212fc8bceb93ca7a9274d2f
Samnang-Sokhorm <sokhornsamnang@gmail.com>
2025-11-23 12:27:27 +0700

    first commit

---
+++ lib/week3/basic.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  print("List <Sting> $args: $args");
+  int a = 6;
+  int b = 3;
+  int sum = a + b;
+  int differince = a - b;
+  int multiply = a * b;
+  double divide = a / b;
+  print("Sum: $sum");
+  print("Differince: $differince");
+  print("Multiply: $multiply");
+  print("Divide: $divide");
+
+}
